9b35cdf5bbc90b374d065adfb21b0ac497c358ce
Brad Van Dyk <bradvandyk@gmail.com>
2025-02-03 14:48:49 -0500

    init xcode project

---
+++ FetchRecipes/FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Brad Van Dyk on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRecipes/FetchRecipesTests/FetchRecipesTests.swift
@@ -0,0 +1,16 @@
+//
+//  FetchRecipesTests.swift
+//  FetchRecipesTests
+//
+//  Created by Brad Van Dyk on 2/3/25.
+//
+
+import Testing
+
+struct FetchRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
